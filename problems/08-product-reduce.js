9e2b4cefae2b87540ac79c10b170facec192b124
Preston <ba6stu13@gmail.com>
2023-08-12 18:39:30 -0400

    problem #8 done

--- problems/08-product-reduce.js
+++ problems/08-product-reduce.js
@@ -17,7 +17,11 @@ console.log(productWithReduce([4, 3])); // 12
 
 let productWithReduce = function(nums) {
     // Your code here
+    return nums.reduce((sum, num) => sum * num);
 }
+
+console.log(productWithReduce([10, 3, 5, 2])); // 300
+console.log(productWithReduce([4, 3])); // 12
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -25,4 +29,4 @@ try {
   module.exports = productWithReduce;
 } catch (e) {
   module.exports = null;
-}
+}
