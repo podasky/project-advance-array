8db437946f6c5fd7af9037f9a4a31138b63e91ca
Preston <ba6stu13@gmail.com>
2023-08-12 18:16:56 -0400

    and problem 6 done

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -14,7 +14,11 @@ console.log(sumWithReduce([])); // 0
 
 let sumWithReduce = function(nums) {
   // Your code here
+  return nums.reduce((sum, num) => sum + num, 0)
 };
+
+console.log(sumWithReduce([10, 3, 5, 2])); // 20
+console.log(sumWithReduce([])); // 0
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
