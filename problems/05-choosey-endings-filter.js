0647426ae638cc6344f9e5701d68f70689561ead
Preston <ba6stu13@gmail.com>
2023-08-12 18:07:54 -0400

    Top Ramen is freaking gross

--- problems/05-choosey-endings-filter.js
+++ problems/05-choosey-endings-filter.js
@@ -27,7 +27,26 @@ console.log(chooseyEndings(17, 'ily'));
 
 let chooseyEndings = function(words, suffix) {
   // Your code here
+  if (!Array.isArray(words)){
+    return []
+  }
+  return words.filter((end) => end.endsWith(suffix))
 };
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'ly'));
+// [ 'family', 'fly', 'timidly' ]
+
+console.log(chooseyEndings(['family', 'hound', 'catalyst', 'fly', 'timidly', 'bond'], 'nd'));
+// [ 'hound', 'bond' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'icity'));
+// [ 'simplicity', 'felicity' ]
+
+console.log(chooseyEndings(['simplicity', 'computer', 'felicity'], 'ily'));
+// [ ]
+
+console.log(chooseyEndings(17, 'ily'));
+// [ ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -35,4 +54,4 @@ try {
   module.exports = chooseyEndings;
 } catch (e) {
   module.exports = null;
-}
+}
